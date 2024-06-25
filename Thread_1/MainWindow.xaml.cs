bcdede0519094455a1810d0d1d5f2673682a6bc2
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-06-25 23:56:08 +0300

    initiall commit

--- Thread_1/MainWindow.xaml.cs
+++ Thread_1/MainWindow.xaml.cs
@@ -1,24 +1,113 @@
-﻿using System.Text;
+﻿using System;
+using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Thread_1
+namespace ProcessApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private Process _process;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void StartAndWaitProcessButton_Click(object sender, RoutedEventArgs e)
+        {
+            _process = new Process();
+            _process.StartInfo.FileName = "notepad.exe";
+            _process.Start();
+            _process.WaitForExit();
+
+            int exitCode = _process.ExitCode;
+            MessageBox.Show($"Process exited with code: {exitCode}");
+        }
+
+        private void StartProcessButton_Click(object sender, RoutedEventArgs e)
+        {
+            _process = new Process();
+            _process.StartInfo.FileName = "notepad.exe";
+            _process.Start();
+        }
+
+        private void WaitForProcessButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_process != null)
+            {
+                _process.WaitForExit();
+                int exitCode = _process.ExitCode;
+                MessageBox.Show($"Process exited with code: {exitCode}");
+            }
+        }
+
+        private void KillProcessButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_process != null && !_process.HasExited)
+            {
+                _process.Kill();
+                MessageBox.Show("Process was killed.");
+            }
+        }
+
+        private void StartProcessWithArgumentsButton_Click(object sender, RoutedEventArgs e)
+        {
+            _process = new Process();
+            _process.StartInfo.FileName = "PathToChildProcess.exe";
+            _process.StartInfo.Arguments = "7 3 +";
+            _process.Start();
+            _process.WaitForExit();
+
+            int exitCode = _process.ExitCode;
+            MessageBox.Show($"Process exited with code: {exitCode}");
+        }
+
+        private void StartSearchProcessButton_Click(object sender, RoutedEventArgs e)
+        {
+            string filePath = @"E:\someFolder\someFile.txt";
+            string searchTerm = "bicycle";
+
+            _process = new Process();
+            _process.StartInfo.FileName = "PathToSearchProcess.exe";
+            _process.StartInfo.Arguments = $"{filePath} {searchTerm}";
+            _process.Start();
+            _process.WaitForExit();
+
+            int exitCode = _process.ExitCode;
+            MessageBox.Show($"Process exited with code: {exitCode}");
+        }
+
+        private void LaunchApplication(string appName)
+        {
+            try
+            {
+                Process.Start(appName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to start application: {ex.Message}");
+            }
+        }
+
+        private void LaunchNotepadButton_Click(object sender, RoutedEventArgs e)
+        {
+            LaunchApplication("notepad.exe");
+        }
+
+        private void LaunchCalculatorButton_Click(object sender, RoutedEventArgs e)
+        {
+            LaunchApplication("calc.exe");
+        }
+
+        private void LaunchPaintButton_Click(object sender, RoutedEventArgs e)
+        {
+            LaunchApplication("mspaint.exe");
+        }
+
+        private void LaunchCustomAppButton_Click(object sender, RoutedEventArgs e)
+        {
+            string customAppPath = CustomAppPathTextBox.Text;
+            LaunchApplication(customAppPath);
         }
     }
 }
